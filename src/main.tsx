b022d5c8e290897d326a570c6f949cce6ee9f933
WorldSalad <worldsaladdev@gmail.com>
2026-06-21 16:47:57 -1000

    update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import "cesium/Build/Cesium/Widgets/widgets.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/KyrsunderProject">
       <App />
     </BrowserRouter>
   </StrictMode>
